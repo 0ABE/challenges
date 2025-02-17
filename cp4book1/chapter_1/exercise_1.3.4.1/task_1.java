8c70a5b0e80109415e78cd23f5c30fe1d9857a4d
Abe Mishler <0abe@mishleremail.com>
2025-02-16 19:41:09 -0500

    Add license headers

--- cp4book1/chapter_1/exercise_1.3.4.1/task_1.java
+++ cp4book1/chapter_1/exercise_1.3.4.1/task_1.java
@@ -1,4 +1,19 @@
-// The task specifies to use Java.
+/**
+ *  .d8888b.        d8888 888888b.   8888888888
+ * d88P  Y88b      d88888 888  "88b  888
+ * 888    888     d88P888 888  .88P  888
+ * 888    888    d88P 888 8888888K.  8888888
+ * 888    888   d88P  888 888  "Y88b 888
+ * 888    888  d88P   888 888    888 888
+ * Y88b  d88P d8888888888 888   d88P 888
+ *  "Y8888P" d88P     888 8888888P"  8888888888
+ *
+ * Copyright (c) 2025, Abe Mishler
+ * Licensed under the Universal Permissive License v 1.0
+ * as shown at https://oss.oracle.com/licenses/upl/.
+ *
+ * The task specifies to use Java (see task_1.java).
+ */
 
 import java.util.*;
 
